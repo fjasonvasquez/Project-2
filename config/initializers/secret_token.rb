3bdf193b5dfffa04f05919418078fa5adeeb0603
Felino Jason Vasquez <felinojasonvasquez@Felinos-MacBook-Air.local>
2013-10-18 17:39:54 -0700

    Project 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitness::Application.config.secret_key_base = '74907643f09dc10804a2fa7bfe56d9ab410e586dbaba9e94e527ca99b620e4f79635a9f99f7a874aff1599ae07c70882927c743403e8d18206362ced162464fd'
