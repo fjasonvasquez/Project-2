e085db41bdd73dcf9eac84125e76d57be934c907
Jason Vasquez <felinojasonvasquez@Felinos-MacBook-Air.local>
2013-10-28 23:15:40 -0700

    This should have been saved first for Create user working on genghis and Rails with a route to workouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,23 +7,23 @@ class User
 	field :password, type: String
 	field :password_confirmation, type: String
 
-before_save :hashed_password
-validates :email, presence: true
-validates :email, uniqueness: { case_sensitive: false }
-validates :password, confirmation: true
+# before_save :hashed_password
+# validates :email, presence: true
+# validates :email, uniqueness: { case_sensitive: false }
+# validates :password, confirmation: true
 
-def authenticate(password)
-	self.hashed_password ==
-	BCrypt::Engine.hash_secret(password, self.salt)
-end
+# def authenticate(password)
+# 	self.hashed_password ==
+# 	BCrypt::Engine.hash_secret(password, self.salt)
+# end
 
-private
-def hash_password
-	if password.present?
-		self.salt = BCrypt::Engine.generate_salt
-		self.hashed_password =
-		BCrypt::Engine.hash_secret(password, self.salt)
-		password = password_confirmation = nil
-	end
-end
+# private
+# def hash_password
+# 	if password.present?
+# 		self.salt = BCrypt::Engine.generate_salt
+# 		self.hashed_password =
+# 		BCrypt::Engine.hash_secret(password, self.salt)
+# 		password = password_confirmation = nil
+# 	end
+# end
 end
